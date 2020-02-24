9daf0a512122aadbd1c88fc0128e067d70df1e5b
anoushatayan-scdm <anoush.atayan@deloitte-audit-analytics.com>
2020-02-24 12:47:26 +0400

    dropzone handling

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,15 +1,13 @@
-$(document).ready(function() {
+Dropzone.options.idDropzone = {
+    maxFilesize: 10, // MB
+    thumbnailMethod: 'contain',
+    maxFiles: 4,
+    acceptedFiles: 'image/*',
+    uploadMultiple: true,
+    addRemoveLinks: true,
+};
+
+$(document).ready(function () {
     "use strict";
 
-    Dropzone.autoDiscover = false;
-    Dropzone.options.id_dropzone = {
-        paramName: "file", // The name that will be used to transfer the file
-        maxFilesize: 2, // MB
-        maxFiles: 4,
-        thumbnailWidth: 300,
-        thumbnailHeight: 400,
-        acceptedFiles: "image/jpeg,image/png,image/gif",
-        uploadMultiple: true,
-        addRemoveLinks: true,
-    };
 });

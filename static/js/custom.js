12a59bfbcac3e6078694331d0e831a671d541132
anoushatayan-scdm <anoush.atayan@deloitte-audit-analytics.com>
2020-02-24 16:41:16 +0400

    added loader and form submit

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,41 @@
+function run_waitMe(effect) {
+    $('body').waitMe({
+        effect: effect,
+        text: 'Extracting',
+        bg: 'rgba(255,255,255,0.7)',
+        color: '#09b9d6',
+    });
+}
+
 Dropzone.options.idDropzone = {
     maxFilesize: 10, // MB
-    thumbnailMethod: 'contain',
+    thumbnailMethod: "contain",
     maxFiles: 4,
-    acceptedFiles: 'image/*',
+    acceptedFiles: "image/*",
     uploadMultiple: true,
     addRemoveLinks: true,
+    autoProcessQueue: false,
+
+    error: function (file, message, xhr) {
+        if (xhr == null) this.removeFile(file);
+        $("#id_message").text(message);
+        $("#id_limit").modal("show");
+    },
+
+    init: function () {
+        var submit_button = document.querySelector("#id_extract_data");
+        var dz = this;
+        submit_button.addEventListener("click", function () {
+            dz.processQueue();
+            run_waitMe("bounce");
+        });
+        dz.on("addedfile", function (file) {
+            $("#id_extract_data").show();
+        });
+        dz.on("removedfile", function (file) {
+            if (dz.getQueuedFiles().length === 0) {
+                $("#id_extract_data").hide();
+            }
+        });
+    }
 };
-
-$(document).ready(function () {
-    "use strict";
-
-});
